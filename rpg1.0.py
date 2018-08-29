7b2a2bd7f0335d618121cd9ac2496796bb7de75c
Peter <Pete5x5@users.noreply.github.com>
2018-08-29 17:21:24 -0400

    topping name now a function, beginning of functionality for topping list revision, moved dupe topping check down

--- rpg1.0.py
+++ rpg1.0.py
@@ -4,6 +4,13 @@ As the dice will it, it shall be done
 
 How many pizzas would you like generated?''')
 
+def intcheck(var1):
+    try:
+         var1 = int(var1)
+    except ValueError:
+        print('Invalid input')
+        continue
+
 while True:
     pizzas = input()
     try:
@@ -57,50 +64,94 @@ while True:
         else:
             break
 
+toplist = []
+
+def topname():
+    global toplist
+    while True:
+            currtop = input()
+            if len(currtop) > 50:
+                print('Topping name too long')
+                continue
+            elif currtop in toplist:
+                print('You already have that topping')
+                continue
+            else:
+                toplist += [currtop]
+                break
+
+for iv1 in range(alltops):
+    print('Enter topping number ' + str(iv1+1))
+    topname()
+
+listcorrect = 0
+
+while listcorrect < 1:
+    print ("\nIs this topping list correct?")
+    for iv112 in range(len(toplist)):
+        print (str(iv112+1) + " - " + str(toplist[iv112]))
+    print('''
+    a - Yes, that's perfect
+    b - I need to add a topping
+    c - I need to remove a topping\n''')
+    while True:
+        listconfirm = input()
+        if listconfirm == 'a':
+            listcorrect == 1
+            break
+        elif listconfirm == 'b':
+            print('Please enter the additional topping now:')
+            topname()
+            break
+        elif listconfirm == 'c':
+            removetop = 0
+            print('Which topping number would you like to remove?')
+            while True:
+                removetop = input()
+                try:
+                    removetop = int(removetop)
+                except ValueError:
+                    print('Invalid input')
+                    continue
+                else:
+                    if removetop < 1 or removetop > len(toplist):
+                        print('Invalid input')
+                        continue
+                    else:
+                        break
+            ######
+        else:
+            print('Invalid choice')
+            continue
+
 if alltops >= tops:
     print('''Would you like to allow duplicate toppings?
-    1 - Yes, that is how the dice will it
-    2 - No, triple olives is gross\n''')
+    a - Yes, that is how the dice will it
+    b - No, triple olives is gross\n''')
 
     while True:
         dupetops = input()
-        if dupetops == '1' or dupetops == '2':
+        if dupetops == 'a' or dupetops == 'b':
             break
         else:
             print('Invalid choice')
             continue
 else:
-    dupetops == '1'
-
-toplist = []
-
-for iv1 in range(alltops):
-    print('Enter topping number ' + str(iv1+1))
-    while True:
-        currtop = input()
-        if len(currtop) > 50:
-            print('Topping name too long')
-            continue
-        elif currtop in toplist:
-            print('You already have that topping')
-            continue
-        else:
-            toplist += [currtop]
-            break
+    dupetops == 'a'
 
 print('''Would you like to randomize the type of sauce for each pizza?
-1 - Yes, true random is the only way
-2 - No, I can only handle regular pizza sauce\n''')
+a - Yes, true random is the only way
+b - No, I can only handle regular pizza sauce\n''')
 
 while True:
     randsauce = input()
-    if randsauce == '1' or randsauce == '2':
+    if randsauce == 'a' or randsauce == 'b':
         break
     else:
         print('Invalid choice')
         continue
 
-if randsauce == '1':
+if randsauce == 'a':
     print('How many sauces are there to choose from?')
     while True:
         allsauce = input()
@@ -120,7 +171,7 @@ if randsauce == '1':
             else:
                 break
 
-if randsauce == '1':
+if randsauce == 'a':
     saucelist = []
     for iv12 in range(allsauce):
         print('Enter sauce number ' + str(iv12+1))
@@ -142,11 +193,11 @@ for iv2 in range(pizzas):
     pizzagen = []
     selectedtops = 0
     print('\nPizza #' + str(iv2+1))
-    if randsauce == '1':
+    if randsauce == 'a':
         print('[sauce = '  + saucelist[random.randint(0,allsauce-1)] + ']')
     while selectedtops < tops:
         temptop = toplist[random.randint(0,alltops-1)]
-        if dupetops == '2' and temptop in pizzagen:
+        if dupetops == 'b' and temptop in pizzagen:
             continue
         else:
             pizzagen += [temptop]
